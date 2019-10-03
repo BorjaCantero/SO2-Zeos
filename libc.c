3c139cff4b015f4bd558b6b6ae2b0b2e0a4ec27d
borja.cantero <alumne@a5s111pc34.fib.upc.es>
2019-10-03 11:17:22 +0200

    funció write no acabada

--- libc.c
+++ libc.c
@@ -43,3 +43,19 @@ int strlen(char *a)
   return i;
 }
 
+int write(int fd, char * buffer, int size){
+  int resultat;
+  __asm__ volatile(
+          "movl %1, %%ebx;"
+          "movl %2, %%ecx;"
+          "movl %3, %%edx;"
+          "movl $4, %%eax;"
+          "int $0x80;"
+          "movl %%eax, %0;"
+          : "=g" (resultat)
+          : "g" (fd), "g"(buffer), "g" (size)
+          : "ax", "bx", "cx", "dx"
+    );
+
+}
+
